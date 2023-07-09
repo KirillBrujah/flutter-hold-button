439016155198dd930de0610e84e94f3f31f37287
kirillbrujah <kirillbrujah@gmail.com>
2023-07-09 23:25:10 +0200

    Initialize Project

---
+++ lib/flutter_hold_button.dart
@@ -0,0 +1,119 @@
+library flutter_hold_button;
+
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class HoldButton extends StatefulWidget {
+  const HoldButton({
+    Key? key,
+    this.size = const Size.fromHeight(40),
+    required this.onTap,
+    required this.duration,
+    required this.child,
+  }) : super(key: key);
+
+  final Size size;
+  final VoidCallback onTap;
+  final Duration duration;
+  final Widget child;
+
+  @override
+  State<HoldButton> createState() => _HoldButtonState();
+}
+
+class _HoldButtonState extends State<HoldButton> {
+  Duration _currentDuration = const Duration();
+  Timer? _timer;
+  bool _isActive = false;
+
+  void _reset() {
+    setState(() {
+      _currentDuration = const Duration();
+    });
+  }
+
+  void _start() {
+    setState(() {
+      _isActive = true;
+    });
+    _timer = Timer.periodic(
+      const Duration(seconds: 1),
+      (t) {
+        setState(() {
+          _currentDuration += const Duration(seconds: 1);
+
+          if (_currentDuration >= widget.duration) {
+            stop();
+            widget.onTap();
+          }
+        });
+      },
+    );
+  }
+
+  void stop() {
+    _timer?.cancel();
+    setState(() {
+      _isActive = false;
+    });
+  }
+
+  void _tapUp(TapUpDetails details) {
+    stop();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ConstrainedBox(
+      constraints: const BoxConstraints(
+        minHeight: 100,
+        minWidth: 100,
+        maxHeight: 100,
+        maxWidth: double.infinity,
+      ),
+      child: GestureDetector(
+        onTapDown: _tapDown,
+        onTapUp: _tapUp,
+        child: Stack(
+          children: [
+            AnimatedScale(
+              duration: _isActive ? widget.duration : const Duration(),
+              scale: _isActive ? 0.8 : 1,
+              child: Container(
+                decoration: _isActive
+                    ? BoxDecoration(
+                        border: Border.all(
+                          color: Theme.of(context)
+                              .colorScheme
+                              .primary
+                              .withOpacity(.7),
+                          width: 3,
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+                      )
+                    : null,
+              ),
+            ),
+            AnimatedScale(
+              duration: const Duration(milliseconds: 75),
+              scale: _isActive ? 0.8 : 1,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.primary,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Center(child: widget.child),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _tapDown(TapDownDetails details) {
+    _reset();
+    _start();
+  }
+}
---
+++ test/flutter_hold_button_test.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flutter_hold_button/flutter_hold_button.dart';
+
+void main() {}
